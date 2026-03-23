a957d46cc875ac286938ff260356bf8d59026c3f
Anton <stakan993@gmail.com>
2026-03-24 00:18:06 +0800

    Добавьте файлы проекта.

---
+++ HrefParser/HrefDataModel.cs
@@ -0,0 +1,17 @@
+﻿namespace HrefParser
+{
+    public enum Status
+    {
+        Pending,
+        InProgress,
+        Completed,
+        Failed
+    }
+    public class HrefDataModel
+    {
+        //ссылка, заголовок сайта по этой ссылке и текущий статус запроса этой ссылки
+        public Uri Href {  get; set; }
+        public string SiteName { get; set; }
+        public Status Status { get; set; }
+    }
+}
---
+++ HrefParser/MainWindowViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace HrefParser
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string name) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+    public class MainWindowViewModel() : BaseViewModel
+    {
+        public string Href
+        {
+            get;
+            set
+            {
+                field = value;
+                OnPropertyChanged(nameof(Href));
+            }
+        }
+        public int Progress
+        {
+            get;
+            set
+            {
+                field = value;
+                OnPropertyChanged(nameof(Progress));
+            }
+        }
+
+        public ObservableCollection<HrefDataModel> Data { get; set; } = new ObservableCollection<HrefDataModel>();
+
+        public ICommand MyButtonClick => field ??= new RelayCommand(Click);
+
+
+        private void Click()
+        {
+            System.Windows.MessageBox.Show($"{Href}");
+        }
+    }
+}
